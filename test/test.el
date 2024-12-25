a614141eded006f62c240f6d6131ab4dee37b53a
Brent Westbrook <brentrwestbrook@gmail.com>
2024-12-25 13:39:58 -0500

    allow passing a different org file to `with-test-org`

--- test/test.el
+++ test/test.el
@@ -11,16 +11,18 @@
 
 (alert-define-style 'test-alert :notifier #'test-alert-notify)
 
-(cl-defmacro with-test-org (&rest body)
-  `(let ((org-directory ".")
-		 (org-agenda-files (list "test.org"))
-		 (alert-default-style 'test-alert)
-		 ;; TODO the fact that I have to include this from my own config is a
-		 ;; really bad sign for the default value in the package
-		 (org-alert-match-string
-		  "SCHEDULED<\"<yesterday>\"+SCHEDULED<\"<tomorrow>\""))
-	 ,@body
-	 (test-alert-reset)))
+(cl-defmacro with-test-org (agenda-file &rest body)
+  (declare (indent defun))
+  (let ((agenda-file (or agenda-file "test.org")))
+	`(let ((org-directory ".")
+		   (org-agenda-files (list ,agenda-file))
+		   (alert-default-style 'test-alert)
+		   ;; TODO the fact that I have to include this from my own config is a
+		   ;; really bad sign for the default value in the package
+		   (org-alert-match-string
+			"SCHEDULED<\"<yesterday>\"+SCHEDULED<\"<tomorrow>\""))
+	   ,@body
+	   (test-alert-reset))))
 
 (defmacro with-current-time (time &rest body)
   "Override `current-time` to return `Sat May 20 09:40:01 2023`, 15 minutes
@@ -35,7 +37,7 @@ before the scheduled event in `test.org`."
 (ert-deftest org-alert-custom-cutoff ()
   "checks that we can extract the correct cutoff from the
  PROPERTIES of a subtree"
-  (with-test-org
+  (with-test-org nil
     (should (equal
 			 '(("remindern test" "09:55" 15))
 			 (org-alert--map-entries 'org-alert--parse-entry)))))
@@ -45,34 +47,34 @@ before the scheduled event in `test.org`."
 
 This works because the default `org-alert-notify-after-event-cutoff` is
 nil, so any time in the past will be alerted."
-  (with-test-org
-   (org-alert-check)
-   (should (= (length test-alert-notifications) 1))))
+  (with-test-org nil
+	(org-alert-check)
+	(should (= (length test-alert-notifications) 1))))
 
 (ert-deftest check-alert-none-cutoff ()
   "Check that `org-alert-check` does not send an alert from `test.org` with
 a post-event cutoff set."
-  (with-test-org
-   (let ((org-alert-notify-after-event-cutoff 60))
-   (org-alert-check)
-   (should (= (length test-alert-notifications) 0)))))
+  (with-test-org nil
+	(let ((org-alert-notify-after-event-cutoff 60))
+	  (org-alert-check)
+	  (should (= (length test-alert-notifications) 0)))))
 
 (ert-deftest check-alert-some-remindern ()
   "Check that `org-alert-check` sends an alert from `test.org` with
 a post-event cutoff set but the current time set appropriately."
-  (with-test-org
-   (with-current-time nil
-	 (let ((org-alert-notify-after-event-cutoff 60))
-	   (org-alert-check)
-	   (should (= (length test-alert-notifications) 1))))))
+  (with-test-org nil
+	(with-current-time nil
+	  (let ((org-alert-notify-after-event-cutoff 60))
+		(org-alert-check)
+		(should (= (length test-alert-notifications) 1))))))
 
 (ert-deftest check-alert-none-remindern ()
   "Check that `org-alert-check` sends an alert from `test.org` with
 a post-event cutoff set but the current time set appropriately."
-  (with-test-org
-   ;; (current-time-string '(25704 52655 0 0)) => "Sat May 20 09:39:59 2023" or
-   ;; just before the notification should trigger
-   (with-current-time (25704 52655 0 0)
-	 (let ((org-alert-notify-after-event-cutoff 60))
-	   (org-alert-check)
-	   (should (= (length test-alert-notifications) 0))))))
+  (with-test-org nil
+	;; (current-time-string '(25704 52655 0 0)) => "Sat May 20 09:39:59 2023" or
+	;; just before the notification should trigger
+	(with-current-time (25704 52655 0 0)
+	  (let ((org-alert-notify-after-event-cutoff 60))
+		(org-alert-check)
+		(should (= (length test-alert-notifications) 0))))))
