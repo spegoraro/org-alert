7161fbc5948393884b19299de34914e743a2786c
Brent Westbrook <brentrwestbrook@gmail.com>
2024-12-25 13:56:26 -0500

    add melpa

--- ci.sh
+++ ci.sh
@@ -3,6 +3,7 @@
 set -xe
 
 emacs -batch -f package-initialize \
+	  --eval '(add-to-list (quote package-archives) (quote ("melpa" . "http://melpa.org/packages/")))' \
 	  --eval '(use-package alert :ensure t)'
 
 cd test && make test
