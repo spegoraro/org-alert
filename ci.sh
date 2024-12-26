8e44a508a7d0c73219eddd8fbd5bab550ed596b4
Ricky Anderson <ricky.anderson2696@gmail.com>
2024-12-26 10:51:42 +0700

    update ci.sh dependencies

--- ci.sh
+++ ci.sh
@@ -4,6 +4,8 @@ set -xe
 
 emacs -batch -f package-initialize \
 	  --eval '(add-to-list (quote package-archives) (quote ("melpa" . "http://melpa.org/packages/")))' \
-	  --eval '(use-package alert :ensure t)'
+	  --eval '(use-package alert :ensure t)' \
+	  --eval '(use-package org-ql :ensure t)' \
+	  --eval '(use-package ts :ensure t)'
 
 cd test && make test
